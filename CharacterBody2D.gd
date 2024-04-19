e2d2d841635c006c1d69cac8aaa084a91a371c65
PauTorrass <ptorrasesteve07@gmail.com>
2024-04-19 14:38:48 +0200

    Rotació CharacterBody

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -12,13 +12,20 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+		var tween = create_tween()
+		tween.tween_property($AnimatedSprite2D, "rotation_degrees", $AnimatedSprite2D.rotation_degrees - fmod($AnimatedSprite2D.rotation_degrees, 90) + 2*90, 0.8).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	position.x += SPEED * delta
+	
+	
+	
 	 
 	move_and_slide()
 	
