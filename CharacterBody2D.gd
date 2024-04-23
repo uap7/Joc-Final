3c871adbde806d8c7bf9b62969177db8eaf1a7d4
PauTorrass <ptorrasesteve07@gmail.com>
2024-04-23 09:37:08 +0200

    Canvi fons

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -16,14 +16,17 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
+		
 		var tween = create_tween()
 		tween.tween_property($AnimatedSprite2D, "rotation_degrees", $AnimatedSprite2D.rotation_degrees - fmod($AnimatedSprite2D.rotation_degrees, 90) + 2*90, 0.6).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	position.x += SPEED * delta
-	
-	
-	
+	velocity.x = SPEED
 	 
 	move_and_slide()
+	
+	if is_zero_approx(velocity.x):
+		mor()
 
+func mor():
+	print('Uix: %.2f' % [velocity.x])
